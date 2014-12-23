560eb8d40c7a7917db8a608959ee96c90aee4b62
Connor Hindley <conn.hindley@gmail.com>
2014-12-23 03:37:04 -0500

    remove htmlmin options

--- Gruntfile.js
+++ Gruntfile.js
@@ -275,15 +275,6 @@ module.exports = function(grunt) {
       options: {
         // This should be the name of your apps angular module
         module: 'grtyrApp',
-        htmlmin: {
-          collapseBooleanAttributes: true,
-          collapseWhitespace: true,
-          removeAttributeQuotes: true,
-          removeEmptyAttributes: true,
-          removeRedundantAttributes: true,
-          removeScriptTypeAttributes: true,
-          removeStyleLinkTypeAttributes: true
-        },
         usemin: 'app/app.js'
       },
       main: {
