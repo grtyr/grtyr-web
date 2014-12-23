e536c1e17bce562e7f2362b198155aa8062262fe
Connor Hindley <conn.hindley@gmail.com>
2014-12-23 03:34:55 -0500

    fix node port & ip

--- server/config/environment/production.js
+++ server/config/environment/production.js
@@ -4,12 +4,12 @@
 // =================================
 module.exports = {
   // Server IP
-  ip: process.env.OPENSHIFT_NODEJS_IP ||
+  ip: process.env.OPENSHIFT_NODEDIY_IP ||
     process.env.IP ||
     undefined,
 
   // Server port
-  port: process.env.OPENSHIFT_NODEJS_PORT ||
+  port: process.env.OPENSHIFT_NODEDIY_PORT ||
     process.env.PORT ||
     8080,
 
