48d39832e1ddf2bf6905fe48581113f12204a0e1
Connor Hindley <conn.hindley@gmail.com>
2014-12-23 03:32:41 -0500

    remove serve-favicon

--- server/config/express.js
+++ server/config/express.js
@@ -5,7 +5,6 @@
 'use strict';
 
 var express = require('express');
-var favicon = require('serve-favicon');
 var morgan = require('morgan');
 var compression = require('compression');
 var bodyParser = require('body-parser');
@@ -42,7 +41,6 @@ module.exports = function(app) {
   app.set('appPath', path.join(config.root, 'client'));
 
   if ('production' === env) {
-    app.use(favicon(path.join(config.root, 'client', 'favicon.ico')));
     app.use(express.static(app.get('appPath')));
     app.use(morgan('dev'));
   }
