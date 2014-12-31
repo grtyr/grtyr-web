d65dccff814cbbb8a62c2578cc2f059b0c7cacba
Connor Hindley <conn.hindley@gmail.com>
2014-12-30 23:01:35 -0500

    admin page title

--- client/app/admin/admin.js
+++ client/app/admin/admin.js
@@ -6,6 +6,7 @@ angular.module('grtyrApp')
       .state('admin', {
         url: '/admin',
         templateUrl: 'app/admin/admin.html',
-        controller: 'AdminCtrl'
+        controller: 'AdminCtrl',
+        title: 'Admin'
       });
   });
